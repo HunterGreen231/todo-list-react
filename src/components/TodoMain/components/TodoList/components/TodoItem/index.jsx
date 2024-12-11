12573166c5988a160a08701316293f42d809f05b
Hunter Green <webdev.huntergreen@gmail.com>
2024-12-11 13:28:51 -0700

    Fixed checkbox bug with local storage.

--- src/components/TodoMain/components/TodoList/components/TodoItem/index.jsx
+++ src/components/TodoMain/components/TodoList/components/TodoItem/index.jsx
@@ -2,10 +2,9 @@ import useStore from '~/store'
 
 export default function TodoItem(props) {
     const { deleteTodo, completeTodo } = useStore();
-
     return (
         <div style={{display: 'flex'}}>
-            <input type="checkbox" onClick={ (event) => completeTodo(event, props.index)} />
+            <input type="checkbox" checked={props.todoItem.checked} onClick={ (event) => completeTodo(event, props.index)} />
             <p className={props.todoItem.checked ? "complete" : ""}>{props.todoItem.text}</p>
             { props.todoItem.checked == true && <button onClick={ () => deleteTodo(props.index) }>Delete</button> }
         </div>
